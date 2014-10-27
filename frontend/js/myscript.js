ad9edac6ba8037adea35779f70bdc321b8a2a577
Zack Wang <ziquan.wang@yahoo.com>
2014-10-26 23:34:42 -0400

    change ui

--- frontend/js/myscript.js
+++ frontend/js/myscript.js
@@ -1,32 +1,48 @@
-var f = 0;
-var pollflag=false;
-var pollctr;
-var counter=0;
+var pollctr = 0;
+var pollid;
 
 var updateCandidatesFlag = true;
-// function updatepoll(result){
-// 	console.log(typeof result);
+var pollflag = true;
 
-// 	var y=Math.floor((Math.random()*3)+1);
-// 	var s;
-// 	if (y === 0) s="647";
-// 	if (y === 1) s="226";
-// 	if (y === 2) s="416";
-// 	if (y === 3) s="519";
-// 	var x1=Math.floor((Math.random()*9)+1);
-// 	var x2=Math.floor((Math.random()*9)+1);
-// 	var x3=Math.floor((Math.random()*9)+1);
-// 	var x4=Math.floor((Math.random()*9)+1);
-// 	var x5=Math.floor((Math.random()*9)+1);
-// 	var x6=Math.floor((Math.random()*9)+1);
-// 	var x7=Math.floor((Math.random()*9)+1);
-// 	$('#poll').html(s+x1+x2+x3+x4+x5+x6+x7);
-// 	counter++;
-// 	if (counter === 100) {
-// 		clearInterval(pollctr);
-// 		$('#poll').html(result[1]+result[2]+result[3]+"******"+result[10])
-// 	}
-// }
+function updatepoll(result){
+	// animation
+	var s;
+	// return a random number between 0 and 3
+	var y = Math.floor((Math.random() * 4)); 
+
+	switch (y) {
+		case 0:
+			s = "647";
+			break;
+		case 1:
+			s = "226";
+			break;
+		case 2:
+			s = "416";
+			break;
+		case 3:
+			s = "519";
+			break;
+	}
+
+	var x1=Math.floor((Math.random()*10));
+	var x2=Math.floor((Math.random()*10));
+	var x3=Math.floor((Math.random()*10));
+	var x4=Math.floor((Math.random()*10));
+	var x5=Math.floor((Math.random()*10));
+	var x6=Math.floor((Math.random()*10));
+	var x7=Math.floor((Math.random()*10));
+
+	$('#poll').html(s+x1+x2+x3+x4+x5+x6+x7);
+	// repeat 100 times
+	pollctr++;
+	
+	// display real lucky audiance
+	if (pollctr === 100) {
+		clearInterval(pollid);
+		$("#poll").html(result.winner[1]+result.winner[2]+result.winner[3]+"XXXXXX"+result.winner[10]);
+	}
+}
 
 function updatevote(result){
 
@@ -36,6 +52,7 @@ function updatevote(result){
 		$("#poll").hide();
 		$("#voting").hide();
 		updateCandidatesFlag = true;
+		pollflag = true;
 	}
 	else if (result.mode === 'vote') {
 		var candidateNum = result.data.candidates.length;
@@ -62,23 +79,29 @@ function updatevote(result){
 
 		if (result.state === "VOTING"){
 			$('h1').html('投票进行中');
-			// hide result row
+			
+			$('#voting').show();
 			$('#total').hide();
 
 			$('#timer').html(result.data.timerRemain);
 
 			// update score
-			$('#c_score0').html(result.data.candidates[0].score);
-			$('#c_score1').html(result.data.candidates[1].score);
+			$('#c_score0').html(result.data.candidates[0].score+" 分");
+			$('#c_score1').html(result.data.candidates[1].score+" 分");
 			if (candidateNum === 3) {
-				$('#c_score2').html(result.data.candidates[2].score);
+				$('#c_score2').html(result.data.candidates[2].score+" 分");
 			}
 			// update votes
-			$('#c_votes0').html(result.data.candidates[0].votes);
-			$('#c_votes1').html(result.data.candidates[1].votes);
+			$('#c_votes0').html(result.data.candidates[0].votes+" 票");
+			$('#c_votes1').html(result.data.candidates[1].votes+" 票");
 			if (candidateNum === 3) {
-				$('#c_votes2').html(result.data.candidates[2].votes);
+				$('#c_votes2').html(result.data.candidates[2].votes+" 票");
 			}
+			// update progress bar
+			var maxVotes = result.data.maxVotes;
+			setProgressBar("#c_prog0", result.data.candidates[0].votes / maxVotes * 100);
+			setProgressBar("#c_prog1", result.data.candidates[1].votes / maxVotes * 100);
+			setProgressBar("#c_prog2", result.data.candidates[2].votes / maxVotes * 100);
 		}
 		else if (result.state === "VOTED"){
 			$('h1').html('投票结束');
@@ -98,22 +121,21 @@ function updatevote(result){
 
 			var t0 = computeTotal(result.data.candidates[0].votes, totalVotes, result.data.candidates[0].score);
 			var t1 = computeTotal(result.data.candidates[1].votes, totalVotes, result.data.candidates[1].score);
-			$('#c_total0').html(t0);
-			$('#c_total1').html(t1);
+			$('#c_total0').html("总分 " + t0);
+			$('#c_total1').html("总分 " + t1);
 			if (candidateNum === 3) {
 				var t2 = computeTotal(result.data.candidates[2].votes, totalVotes, result.data.candidates[2].score);
-				$('#c_total2').html(t2);
+				$('#c_total2').html("总分 " + t2);
 			}
 		}
 	}
-	else if (result.mode === 'poll') {
+	else if (result.mode === 'poll' && pollflag) {
+		pollflag = false;
 		$('h1').html("抽奖");
 		$('#voting').hide();
 		$("#poll").show();
 
-		// TODO
-		// pollctr = setInterval(function(){updatepoll(result.winner)},30);
-	  //$("#poll").html(result.winner[1]+result.winner[2]+result.winner[3]+"XXXXXX"+result.winner[10]);
+		pollid = setInterval(function(){updatepoll(result);},30);
 	}
 }
 
@@ -129,9 +151,16 @@ function update(){
 }
 
 function computeTotal(votes, totalVotes, score) {
+	// 60% from mentor, 40% from audiance, in another word,
+	// a candidate can get 100 if his mentor gives him 100 and 
+	// he gets all votes from audience
 	return votes / totalVotes * 40 + score * 0.6;
 }
 
+function setProgressBar(id, val) {
+  $(id).css('width', val+'%').attr('aria-valuenow', val); 
+}	
+
 $(document).ready(function(){
-	setInterval("update()",5000);
+	setInterval(update,1000);
 });
